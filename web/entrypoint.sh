dd51c18efd32b3f0c0f5a1cf612ebb8101db2250
sursum <chrismakoundoul@gmail.com>
2018-01-15 16:31:00 +0100

    collecting statics

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd newsorg
-echo "DB_NAME=${DB_NAME}"
 python3 manage.py makemigrations 
 python3 manage.py migrate 
-# python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 uwsgi --master --socket 0.0.0.0:8000 --plugins python3 --protocol uwsgi --wsgi newsorg.wsgi:application
 exec "$@"
